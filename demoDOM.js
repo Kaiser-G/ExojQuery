c48b6e8969afff90923bccb7a75e75e0b16c11ec
Kaiser-G <kaiserg6969@gmail.com>
2024-02-20 11:37:35 +0100

    Exo 6

--- demoDOM.js
+++ demoDOM.js
@@ -1,5 +1,44 @@
 // Exercice 6 : exercices jQuery avec séparation du JavaScript et du HTML
 
 $(document).ready(function () {
-  alert("La page est chargée avec la version :" + $.fn.jquery);
+  // alert("La page est chargée avec la version :" + $.fn.jquery);
+
+  function onBtnClickFirst() {
+    $("#divtest").before(
+      $("#nouveau").val("ajouter un nouvel élément en début de liste")
+    );
+  }
+  function onBtnClickBefore() {
+    $("#p2").before(
+      $("#nouveau").val(
+        "ajouter un nouvel élément avant un élément de la liste"
+      )
+    );
+  }
+
+  function onBtnClickAfter() {
+    $("#pcible").after(
+      $("#nouveau").val(
+        "ajouter un nouvel élément après un élément de la liste"
+      )
+    );
+  }
+  function onBtnClickRemove() {
+    $("#pcible").remove();
+  }
+
+  function onBtnClickEmpty() {
+    $("#p2").empty();
+  }
+
+  //création des boutons
+
+  function initComponents() {
+    $("#btnAjoutDebutListe").click(onBtnClickFirst);
+    $("#btnAjoutAvant").click(onBtnClickBefore);
+    $("#btnAjoutApres").click(onBtnClickAfter);
+    $("#btnSup").click(onBtnClickRemove);
+    $("#btnVider").click(onBtnClickEmpty);
+  }
+  $(document).ready(initComponents);
 });
